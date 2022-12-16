f41ad35cebca9e36b0b7d8b3db6e16c1a08c4774
smaranjitghose <46641503+smaranjitghose@users.noreply.github.com>
2022-12-15 22:28:17 -0500

    v0.0.1

---
+++ pages/02_📝_Feedback.py
@@ -0,0 +1,51 @@
+"""
+Contact Page using formsubmit.co API
+"""
+import streamlit as st
+from streamlit_lottie import st_lottie
+from utils import *
+
+
+st.set_page_config(
+        page_title="AI Audio Transciber",
+        page_icon="▶️",
+        layout= "wide",
+        initial_sidebar_state="expanded",
+        menu_items={
+        'Get Help': 'https://github.com/smaranjitghose/AIAudioTranscriber',
+        'Report a bug': "https://github.com/smaranjitghose/AIAudioTranscriber/issues",
+        'About': "## A minimalistic application to generate transcriptions for audio built using Python"
+        } )
+
+
+
+st.title(":mailbox: Get In Touch With Me!")
+hide_footer()
+
+# Load Stylesheet(s) for relevant components
+css_local("assets/styles/contact.css")
+# Load and display animation
+anim = lottie_local("assets/animations/contact.json")
+st_lottie(anim,
+            speed=1,
+            reverse=False,
+            loop=True,
+            quality="medium", # low; medium ; high
+            # renderer="svg", # canvas
+            height=400,
+            width=400,
+            key=None,
+            )
+# HTML code for formsubmit contactform template
+contact_form = """
+            <form action="https://formsubmit.co/b2365a7af4d69269a92869bf9be52f1ba" method="POST">
+            <input type="hidden" name="_captcha" value="true">
+            <input type="hidden" name="_template" value="table">
+            <input type="text" name="name" placeholder="Your name" required>
+            <input type="email" name="email" placeholder="Your email" required>
+            <textarea name="message" placeholder="Your message here"></textarea>
+            <button type="submit">Send</button>
+            </form>
+            """
+st.markdown(contact_form,unsafe_allow_html=True)
+
---
+++ utils.py
@@ -0,0 +1,60 @@
+import streamlit as st
+from streamlit_lottie import st_lottie 
+import json
+
+def css_local(filepath:str):
+    """
+    Method to load the desired stylesheet from the given filepath
+    """ 
+    with open(filepath) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+def lottie_local(filepath: str):
+    """
+    Method to load the desired Lottie Animation from the given filepath
+    """
+    with open(filepath, "r") as f:
+        return json.load(f)
+
+
+def lottie_url(url: str):
+    """
+    Method to load the desired Lottie Animation from given url
+    """
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+def display_map(l1:list=[22.572645],l2:list=[88.363892],z:int=9)->None:
+    """
+    Method to display the desired coordinates in a map by using OpenStreetAPI
+
+    Parameters
+    -----------
+    l1 : list
+         desired latitude coordinate(s); default set for Kolkata ([22.572645])
+    l2 : list
+         desired longitude coordinate(s); default set for Kolkata ([88.363892])
+    z  : int 
+         desired zoom level; default set to metropolitan area level(9) 
+
+    Returns
+    --------
+    None
+    
+    See Also
+    --------
+    For plotting multiple cities, simply pass their respective latitude and longitude coordinates in 
+    the same list
+    """
+    map_data = pd.DataFrame({"latitude":np.array(l1),"longitude":np.array(l2)})
+    st.map(map_data,zoom=z)
+
+def hide_footer():
+    hide_st_style = """
+            <style>
+            footer {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_st_style, unsafe_allow_html=True)
