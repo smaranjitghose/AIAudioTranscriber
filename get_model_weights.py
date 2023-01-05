59800a771e52986b0a6f92f131eec9e8beadc42b
smaranjitghose <46641503+smaranjitghose@users.noreply.github.com>
2023-01-05 07:05:19 -0500

    Modify paths using pathlinb

--- get_model_weights.py
+++ get_model_weights.py
@@ -1,5 +1,5 @@
-import os
 import urllib.request
+from pathlib import Path
 from tqdm import tqdm
 
 # URLs of the model weights files
@@ -9,12 +9,18 @@ urls = [
  "https://openaipublic.azureedge.net/main/whisper/models/345ae4da62f9b3d59415adc60127b97c714f32e89e936602e85993674d08dcb1/medium.pt",
  "https://openaipublic.azureedge.net/main/whisper/models/81f7c96c852ee8fc832187b0132e569d6c3065a3252ed18e56effd0b6a73e524/large-v2.pt"]
 
-# Create the 'models' sub-directory if it does not exist
-if not os.path.exists('./assets/models'):
-    os.makedirs('./assets/models')
+# Create the 'assets/models' sub-directory if it does not exist
+models_dir = Path('assets/models')
+if not models_dir.exists():
+    models_dir.mkdir(parents=True)
 
 # Download the model weights files
-for url in tqdm(urls, desc='Downloading Whisper Model Weights'):
+for url in tqdm(urls, desc='Downloading model weights'):
     file_name = url.split('/')[-1]
-    print(f"Going for {url.split('.')[0]} model")
-    urllib.request.urlretrieve(url, file_path)
+    file_path = models_dir / file_name
+    try:
+        urllib.request.urlretrieve(url, file_path)
+    except Exception as e:
+        print(f'Failed to download model weights from {url}: {e}')
+
+print('Model weights successfully downloaded!')
